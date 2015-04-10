40b4165f5adc892e873b99c713596f340db5f418
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-04-10 16:41:55 -0700

    Autoformatting with webstorm

--- filter-collections-server.js
+++ filter-collections-server.js
@@ -1,112 +1,114 @@
 FilterCollections = {};
 
 FilterCollections._extendedPublishCursor = function (cursor, sub, collection, name) {
-  var observeHandle = cursor.observeChanges({
-    added: function (id, fields) {
-      // Add the name of this filter collection
-      fields.__filter = name;
-      sub.added(collection, id, fields);
-    },
-    changed: function (id, fields) {
-      fields.__filter = name;
-      sub.changed(collection, id, fields);
-    },
-    removed: function (id) {
-      sub.removed(collection, id);
-    }
-  });
+    var observeHandle = cursor.observeChanges({
+        added: function (id, fields) {
+            // Add the name of this filter collection
+            fields.__filter = name;
+            sub.added(collection, id, fields);
+        },
+        changed: function (id, fields) {
+            fields.__filter = name;
+            sub.changed(collection, id, fields);
+        },
+        removed: function (id) {
+            sub.removed(collection, id);
+        }
+    });
 
-  // We don't call sub.ready() here: it gets called in livedata_server, after
-  // possibly calling _publishCursor on multiple returned cursors.
+    // We don't call sub.ready() here: it gets called in livedata_server, after
+    // possibly calling _publishCursor on multiple returned cursors.
 
-  // register stop callback (expects lambda w/ no args).
-  sub.onStop(function () {observeHandle.stop();});
+    // register stop callback (expects lambda w/ no args).
+    sub.onStop(function () {
+        observeHandle.stop();
+    });
 };
 
 FilterCollections.publish = function (collection, options) {
 
-  var self = this;
-
-  options = options || {};
-
-  var callbacks = options.callbacks || {};
-
-  // var cursor = {};
-
-  var name = (options.name) ? options.name : collection._name;
-
-  var publisherResultsId = 'fc-' + name + '-results';
-  var publisherCountId = 'fc-' + name + '-count';
-  var publisherCountCollectionName = name + 'CountFC';
-
-  /**
-   * Publish query results.
-   */
-
-  Meteor.publish(publisherResultsId, function (query) {
-
-    var allow = true;
-
-    if (callbacks.allow && _.isFunction(callbacks.allow))
-      allow = callbacks.allow(query, this);
-
-    if(!allow){
-      throw new Meteor.Error(417, 'Not allowed');
-    }
-
-    query = (query && !_.isEmpty(query)) ? query : {};
-
-    query.selector = query.selector || {};
-
-    query.options = query.options || {
-      sort: [],
-      skip: 0,
-      limit: 10
-    };
-
-    if (callbacks.beforePublish && _.isFunction(callbacks.beforePublish))
-      query = callbacks.beforePublish(query, this) || query;
-
-    var cursor = collection.find(query.selector, query.options);
-
-    if (callbacks.afterPublish && _.isFunction(callbacks.afterPublish))
-      cursor = callbacks.afterPublish('results', cursor, this) || cursor;
-
-    FilterCollections._extendedPublishCursor(cursor, this, collection._name, publisherResultsId);
-  });
-
-  /**
-   * Publish result count.
-   */
-
-  Meteor.publish(publisherCountId, function (query) {
     var self = this;
-    var allow = true;
-    var cursor = {};
 
-    if (callbacks.allow && _.isFunction(callbacks.allow))
-      allow = callbacks.allow(query, this);
+    options = options || {};
 
-    if(!allow){
-      throw new Meteor.Error(417, 'Not allowed');
-    }
+    var callbacks = options.callbacks || {};
 
-    query = (query && !_.isEmpty(query)) ? query : {};
-    query.selector = query.selector || {};
+    // var cursor = {};
 
-    if(callbacks.beforePublish && _.isFunction(callbacks.beforePublish))
-      query = callbacks.beforePublish(query, this) || query;
+    var name = (options.name) ? options.name : collection._name;
 
-    var count = collection.find(query.selector).count() || 0;
+    var publisherResultsId = 'fc-' + name + '-results';
+    var publisherCountId = 'fc-' + name + '-count';
+    var publisherCountCollectionName = name + 'CountFC';
 
-    if(callbacks.afterPublish && _.isFunction(callbacks.afterPublish))
-      cursor = callbacks.afterPublish('count', cursor, this) || cursor;
+    /**
+     * Publish query results.
+     */
 
-    self.added(publisherCountCollectionName, Meteor.uuid(), {
-      count: count,
-      query: query
+    Meteor.publish(publisherResultsId, function (query) {
+
+        var allow = true;
+
+        if (callbacks.allow && _.isFunction(callbacks.allow))
+            allow = callbacks.allow(query, this);
+
+        if (!allow) {
+            throw new Meteor.Error(417, 'Not allowed');
+        }
+
+        query = (query && !_.isEmpty(query)) ? query : {};
+
+        query.selector = query.selector || {};
+
+        query.options = query.options || {
+            sort: [],
+            skip: 0,
+            limit: 10
+        };
+
+        if (callbacks.beforePublish && _.isFunction(callbacks.beforePublish))
+            query = callbacks.beforePublish(query, this) || query;
+
+        var cursor = collection.find(query.selector, query.options);
+
+        if (callbacks.afterPublish && _.isFunction(callbacks.afterPublish))
+            cursor = callbacks.afterPublish('results', cursor, this) || cursor;
+
+        FilterCollections._extendedPublishCursor(cursor, this, collection._name, publisherResultsId);
     });
 
-    this.ready();
-  });
+    /**
+     * Publish result count.
+     */
+
+    Meteor.publish(publisherCountId, function (query) {
+        var self = this;
+        var allow = true;
+        var cursor = {};
+
+        if (callbacks.allow && _.isFunction(callbacks.allow))
+            allow = callbacks.allow(query, this);
+
+        if (!allow) {
+            throw new Meteor.Error(417, 'Not allowed');
+        }
+
+        query = (query && !_.isEmpty(query)) ? query : {};
+        query.selector = query.selector || {};
+
+        if (callbacks.beforePublish && _.isFunction(callbacks.beforePublish))
+            query = callbacks.beforePublish(query, this) || query;
+
+        var count = collection.find(query.selector).count() || 0;
+
+        if (callbacks.afterPublish && _.isFunction(callbacks.afterPublish))
+            cursor = callbacks.afterPublish('count', cursor, this) || cursor;
+
+        self.added(publisherCountCollectionName, Meteor.uuid(), {
+            count: count,
+            query: query
+        });
+
+        this.ready();
+    });
 };
--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  summary: "Filter, paging and sort for Meteor collections with hooks and Iron Router support.",
-  version: "0.2.7",
-  name: "doctorpangloss:filter-collections",
-  git: "https://github.com/workpop/filter-collections"
+    summary: "Filter, paging and sort for Meteor collections with hooks and Iron Router support.",
+    version: "0.2.7",
+    name: "doctorpangloss:filter-collections",
+    git: "https://github.com/workpop/filter-collections"
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0.2.1');
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@1.0.2.1');
 
-  api.use('underscore', ['client', 'server']);
+    api.use('underscore', ['client', 'server']);
 
-  api.addFiles('filter-collections-client.js', ['client']);
-  api.addFiles('filter-collections-server.js', ['server']);
-  api.export('FilterCollections')
+    api.addFiles('filter-collections-client.js', ['client']);
+    api.addFiles('filter-collections-server.js', ['server']);
+    api.export('FilterCollections')
 });
